d9c154463af8c33605e94720726d13c404487521
qiukewei <kewei.qiu@outlook.com>
2020-12-09 20:11:05 +0800

    update code and files for face detection (modify structures of code, separate functions, add comments and test cases)

--- detect_face.py
+++ detect_face.py
@@ -1,78 +1,157 @@
 import cv2
+import copy
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
 
-def rotate(name):
-    # create face_cascade and eye_cascade objects
-    face_cascade=cv2.CascadeClassifier("detect_face/haarcascade_frontalface_default.xml")
-    eye_cascade=cv2.CascadeClassifier("detect_face/haarcascade_eye.xml")
-
+def detect_face(name):
     # load image
+    original = read_image(name)
+    # make deep copy of the image
+    img = copy.deepcopy(original)
+    # convert the image into grayscale
+    gray = convert_to_grayscale(img)
+
+    # detect and show faces in the original image
+    faces = find_face_regions(img, gray)
+    if faces is None:
+        return None
+
+    # rotate and crop each detected face
+    count = 1
+    for (x, y, w, h) in faces:
+        # create regions of interest on both original image and grayscale image
+        roi_color = copy.deepcopy(original[y:y+h, x:x+w])
+        roi_color_copy = copy.deepcopy(roi_color)
+        roi_gray = copy.deepcopy(gray[y:y+h, x:x+w])
+
+        # detect eyes in face region
+        left_eye, right_eye = find_eyes(roi_color, roi_gray)
+        if left_eye is None:
+            return None
+
+        # calculate angle and direction for rotation
+        angle, direction = calculate_angle(left_eye, right_eye, roi_color)
+        # rotate face region
+        rotated = rotate(roi_color_copy, angle, direction)
+        # crop face region
+        cropped = crop_face(rotated)
+        if cropped is None:
+            return None
+
+        # save result to file
+        cv2.imwrite(name + '.face' + str(count) + '.jpg', cropped)
+        count += 1
+
+
+def read_image(name):
+    # load and show image
     img = cv2.imread(name)
-    cv2.imshow('window_name', img)
+    show_image('original image', img)
+
+    return img
+
+
+def show_image(window_name, img):
+    cv2.imshow(window_name, img)
     # waits for user to press any key
     # (this is necessary to avoid Python kernel form crashing)
     cv2.waitKey(0)
     # closing all open windows
     cv2.destroyAllWindows()
 
-    # get height and width of the image
-    h, w = img.shape[:2]
 
+def convert_to_grayscale(img):
     # convert the image into grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    return gray
+
+
+def find_face_regions(img, gray):
+    # create face cascade
+    face_cascade = cv2.CascadeClassifier("detect_face/haarcascade_frontalface_default.xml")
+
     # detect faces in image
-    # 1 face per image for now, implement multi face version later
-    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
-    # draw rectangles around faces
-    for (x, y,  w,  h) in faces:
+    # parameters for detectMultiScale
+    scaleFactor = 1.1
+    minNeighbors = 5
+    # timeout count in case of infinite while loop
+    timeout = 0
+    faces = face_cascade.detectMultiScale(gray, scaleFactor, minNeighbors)
+
+    # no faces detected in image
+    while len(faces) == 0:
+        # time out, failed to detect faces
+        if timeout > 20:
+            print("ERROR: timeout, failed to detect correct faces(s) in given image!")
+            return None
+
+        # adjust parameters and detect again
+        else:
+            timeout += 1
+            minNeighbors += 1
+            faces = face_cascade.detectMultiScale(gray, scaleFactor, minNeighbors)
+
+    # draw rectangles around faces and show the result
+    for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    show_image('faces detected', img)
 
-        center_x = x + h // 2
-        center_y = y + w // 2
+    return faces
 
-    cv2.imshow('window_name', img)
-    # waits for user to press any key
-    # (this is necessary to avoid Python kernel form crashing)
-    cv2.waitKey(0)
-    # closing all open windows
-    cv2.destroyAllWindows()
 
-    # Creating two regions of interest
-    # roi_gray = gray[y:(y + h), x:(x + w)]
-    # roi_color = img[y:(y + h), x:(x + w)]
+def find_eyes(roi_color, roi_gray):
+    # create eye cascade
+    eye_cascade = cv2.CascadeClassifier("detect_face/haarcascade_eye.xml")
 
     # detect eyes in image
-    # 1 pair of eyes per image for now, implement multi face version later
+    # parameters for detectMultiScale
+    scaleFactor = 1.1
     minNeighbors = 5
-    eyes = eye_cascade.detectMultiScale(gray, 1.1, minNeighbors)
-    while eyes.shape[0] > 2:
-        minNeighbors += 1
-        eyes = eye_cascade.detectMultiScale(gray, 1.1, minNeighbors)
-    while eyes.shape[0] < 2:
-        minNeighbors -= 1
-        eyes = eye_cascade.detectMultiScale(gray, 1.1, minNeighbors)
+    # timeout count in case of infinite while loop
+    timeout = 0
+    eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor, minNeighbors)
 
+    # more than 2 eyes detected in given face region
+    while eyes.shape[0] > 2:
+        # time out, failed to detect eyes
+        if timeout > 20:
+            print("ERROR: timeout, failed to detect correct eye(s) in given face region!")
+            return None, None
+
+        # adjust parameters and detect again
+        else:
+            timeout += 1
+            minNeighbors += 1
+            eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor, minNeighbors)
+
+    # less than 2 eyes detected in given face region
+    while eyes.shape[0] < 2:
+        # time out, failed to detect eyes
+        if timeout > 20:
+            print("ERROR: timeout, failed to detect correct eye(s) in given face region!")
+            return None, None
+
+        # adjust parameters and detect again
+        else:
+            timeout += 1
+            minNeighbors -= 1
+            eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor, minNeighbors)
+
+    # get region of detected eyes
     index = 0
-    # divide one eye from another
-    for (ex, ey,  ew,  eh) in eyes:
+    for (ex, ey, ew, eh) in eyes:
         if index == 0:
             eye1 = (ex, ey, ew, eh)
-        elif index == 1:
+        else:
             eye2 = (ex, ey, ew, eh)
 
         # draw rectangles around detected eyes
-        cv2.rectangle(img, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), 3)
+        cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), 3)
         index = index + 1
-        cv2.imshow('window_name', img)
-        # waits for user to press any key
-        # (this is necessary to avoid Python kernel form crashing)
-        cv2.waitKey(0)
-        # closing all open windows
-        cv2.destroyAllWindows()
+        show_image('eyes detected', roi_color)
 
     # differentiate between left eye and right eye
     if eye1[0] < eye2[0]:
@@ -82,7 +161,11 @@ def rotate(name):
         left_eye = eye2
         right_eye = eye1
 
-    # calculate coordinates of a central points of rectangles around eyes
+    return left_eye, right_eye
+
+
+def calculate_angle(left_eye, right_eye, roi_color):
+    # calculate coordinates of central points of rectangles around eyes
     left_eye_center = (int(left_eye[0] + (left_eye[2] / 2)), int(left_eye[1] + (left_eye[3] / 2)))
     left_eye_x = left_eye_center[0]
     left_eye_y = left_eye_center[1]
@@ -92,9 +175,9 @@ def rotate(name):
     right_eye_y = right_eye_center[1]
 
     # draw circles on central points
-    cv2.circle(img, left_eye_center, 5, (255, 0, 0), -1)
-    cv2.circle(img, right_eye_center, 5, (255, 0, 0), -1)
-    cv2.line(img, right_eye_center, left_eye_center, (0, 200, 200), 3)
+    cv2.circle(roi_color, left_eye_center, 5, (255, 0, 0), -1)
+    cv2.circle(roi_color, right_eye_center, 5, (255, 0, 0), -1)
+    cv2.line(roi_color, right_eye_center, left_eye_center, (0, 200, 200), 3)
 
     # detect the third point of the rectangle
     if left_eye_y > right_eye_y:
@@ -107,18 +190,13 @@ def rotate(name):
         direction = 1
 
     # draw circle on the third point of the rectangle
-    cv2.circle(img, A, 5, (255, 0, 0), -1)
+    cv2.circle(roi_color, A, 5, (255, 0, 0), -1)
 
     # draw lines between circles
-    cv2.line(img, right_eye_center, left_eye_center, (0, 200, 200), 3)
-    cv2.line(img, left_eye_center, A, (0, 200, 200), 3)
-    cv2.line(img, right_eye_center, A, (0, 200, 200), 3)
-    cv2.imshow('window_name', img)
-    # waits for user to press any key
-    # (this is necessary to avoid Python kernel form crashing)
-    cv2.waitKey(0)
-    # closing all open windows
-    cv2.destroyAllWindows()
+    cv2.line(roi_color, right_eye_center, left_eye_center, (0, 200, 200), 3)
+    cv2.line(roi_color, left_eye_center, A, (0, 200, 200), 3)
+    cv2.line(roi_color, right_eye_center, A, (0, 200, 200), 3)
+    show_image('triangle system between eyes', roi_color)
 
     # calculate rotation angle
     delta_x = right_eye_x - left_eye_x
@@ -126,60 +204,67 @@ def rotate(name):
     angle = np.arctan(delta_y/delta_x)
     angle = (angle * 180) / np.pi
 
+    return angle, direction
+
+
+def rotate(img, angle, direction):
+    # rotate image using warpAffine (scale is wrong)
+    # get height and width of the image
+    # h, w = img.shape[:2]
     # calculate center point of the image
     # integer division "//"" ensures that we receive whole numbers
     # center = (w // 2, h // 2)
-    center = (center_x, center_y)
-
-    # # get roration matrix M using cv2.getRotationMatrix2D
+    #
+    # # get rotation matrix M using cv2.getRotationMatrix2D
     # M = cv2.getRotationMatrix2D(center, angle, 1.0)
-
-    # load image again
-    img = cv2.imread(name)
-
     # apply the rotation to our image using cv2.warpAffine
     # rotated = cv2.warpAffine(img, M, (w, h))
 
+    # rotate image using Pillow
     img1 = Image.fromarray(img)
     rotated = np.array(img1.rotate(angle))
-    cropped = crop_face(rotated)
 
-    # cv2.imshow('window', rotated)
-    # # waits for user to press any key
-    # # (this is necessary to avoid Python kernel form crashing)
-    # cv2.waitKey(0)
-    # # closing all open windows
-    # cv2.destroyAllWindows()
-
-    cv2.imwrite(name + '.result.jpg', cropped)
+    return rotated
 
 
 def crop_face(rotated):
+    # deep copy of rotated image (to show detected face)
+    rotated_copy = copy.deepcopy(rotated)
     # convert the image into grayscale
     rotated_gray = cv2.cvtColor(rotated, cv2.COLOR_BGR2GRAY)
 
-    # detect faces in image
-    # 1 face per image for now, implement multi face version later
+    # detect face in the image (should be 1)
+    face = find_face_regions(rotated_copy, rotated_gray)
+    # error checking
+    if face is None:
+        return None
+    if face.shape[0] > 1:
+        return None
 
-    face_cascade=cv2.CascadeClassifier("detect_face/haarcascade_frontalface_default.xml")
-    faces = face_cascade.detectMultiScale(rotated_gray, 1.1, 5)
-    # draw rectangles around faces
-    for (x, y, w, h) in faces:
-        # cv2.rectangle(rotated, (x, y), (x + w, y + h), (0, 255, 0), 3)
-
-         cropped = rotated[x:x+w, y:y+h]
-    cv2.imshow('window', cropped)
-    # waits for user to press any key
-    # (this is necessary to avoid Python kernel form crashing)
-    cv2.waitKey(0)
-    # closing all open windows
-    cv2.destroyAllWindows()
+    # crop image according to rectangle around the detected face
+    for (x, y, w, h) in face:
+        cropped = rotated[x:x+w, y:y+h]
+        show_image('cropped face', cropped)
 
     return cropped
 
 
+def test_detect_face():
+    # images with no face
+
+    # images with incomplete face
+    detect_face('detect_face/incomplete1.jpg')
+    detect_face('detect_face/incomplete2.jpg')
+    detect_face('detect_face/incomplete3.jpg')
+
+    # images with single face
+    detect_face('detect_face/single1.jpg')
+    detect_face('detect_face/single2.jpg')
+    detect_face('detect_face/single3.jpg')
+
+    # images with multi faces
+
+
 # main program
 if __name__ == '__main__':
-    rotate('detect_face/ini1.jpg')
-    rotate('detect_face/ini2.jpg')
-    rotate('detect_face/ini3.jpg')
+    test_detect_face()
