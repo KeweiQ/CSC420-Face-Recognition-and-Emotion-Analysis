02fb9efdd0d64970480720fc0fdcbf88e5cda28a
shoko-inori <frank.gong.1006@hotmail.com>
2020-12-09 06:34:05 -0500

    neural network and validation

--- neural_network.py
+++ neural_network.py
@@ -8,6 +8,7 @@ This file includes:
 
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report
+import numpy as np
 import preprocess_dataset
 
 
@@ -17,3 +18,17 @@ if __name__ == '__main__':
     img_train, img_train_label, img_validation, img_validation_label, img_test, img_test_label = \
         preprocess_dataset.split_data(dataset_list)
 
+    # Perform dimensionality reduction on the datasets
+    # TODO: Implement this
+    img_train_reduced = img_train
+    img_validation_reduced = img_validation
+    img_test_reduced = img_test
+
+    # Train the neural network
+    mlp_clf = MLPClassifier(solver='lbfgs', early_stopping=True).fit(img_train_reduced, img_train_label)
+
+    # Predict the validation set to check model accuracy
+    validation_pred = mlp_clf.predict(img_validation_reduced)
+    validation_accuracy = np.average(np.array(
+        [1 if validation_pred[i] == img_validation_label[i] else 0 for i in range(len(validation_pred))]
+    ))
